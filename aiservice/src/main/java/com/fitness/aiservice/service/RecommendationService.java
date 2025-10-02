4eaf00f61ab94efb1ffbbc31d8bf0eef7b3a5c79
Chirag <khoalachirag26@gmail.com>
2025-10-02 17:59:17 +0530

    AI service build, data modeling and apis

---
+++ aiservice/src/main/java/com/fitness/aiservice/service/RecommendationService.java
@@ -0,0 +1,28 @@
+package com.fitness.aiservice.service;
+
+
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Service
+public class RecommendationService {
+    @Autowired
+    private RecommendationRepository recommendationRepository;
+
+
+    public List<Recommendation> getUserRecommendation(String userId) {
+     return recommendationRepository.findByUserId(userId);
+    }
+
+    public Recommendation getActivityRecommendation(String activityId) {
+        return recommendationRepository.findByActivityId(activityId)
+                .orElseThrow(()->new RuntimeException("No RecommendationFound for this activity "+ activityId));
+    }
+}
